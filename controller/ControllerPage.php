e46c9712fc551c7dea3642f1da3c5556fa6122f5
Alex <alex.b-39200@hotmail.fr>
2025-07-21 16:06:56 +0200

    ajout restriction sur controllerPage

--- controller/ControllerPage.php
+++ controller/ControllerPage.php
@@ -1,8 +1,15 @@
 <?php
-class ControllerPage {
-    public function homePage(){
-        $modelUser = new ModelUser();
-        $users = $modelUser->getUsers();
-        require './view/page/homepage.php';
+class ControllerPage
+{
+    public function homePage()
+    {
+        if (isset($_SESSION['id'])) {
+            header('Location: /mangateque/login');
+            exit;
+        } else {
+            $modelUser = new ModelUser();
+            $users = $modelUser->getUsers();
+            require './view/page/homepage.php';
+        }
     }
 }
